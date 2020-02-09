dba432a33ced34d24fa7b538fa670700d1fa4e05
Alex Birch <Birch-san@users.noreply.github.com>
2020-02-09 18:15:25 +0000

    diff and render entities

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,23 +2,81 @@ import {useSelector} from "react-redux";
 import {RootState} from "../store";
 import React from "react";
 import './Grid.css';
+import {eqGrid, eqHunters, eqWolves, Hunter, Wolf} from "../slices";
 
-export const Grid: React.FC = () => {
-  const grid = useSelector((state: RootState) => state.game.grid);
+interface PositionProps {
+  x: number
+  y: number
+}
+interface HunterTileProps extends PositionProps {
+  username: string
+}
+interface WolfTileProps extends PositionProps {}
+
+const PlayerTile: React.FC<WolfTileProps> = ({ x, y, children }) => {
+  return (
+    <div>{children}</div>
+  )
+};
+
+const HunterTile: React.FC<HunterTileProps> = ({ x, y, username }) => {
+  const emoji = ((username: string): string => {
+    if (username.length % 3 === 0) {
+      return '🤷';
+    } else {
+      return '🙎';
+    }
+  })(username);
 
   return (
-    <table className="grid">
-      <tbody>
-      {
-        grid.map((row, ix) => <tr key={ix}>
-          {
-            row.map((val, ix) => <td key={ix}>
-              {val}
-            </td>)
-          }
-        </tr>)
-      }
-      </tbody>
-    </table>
+    <PlayerTile
+      x={x}
+      y={y}
+    >{emoji}
+    </PlayerTile>
+  )
+};
+
+const WolfTile: React.FC<WolfTileProps> = ({ x, y }) => {
+  return (
+    <PlayerTile
+      x={x}
+      y={y}
+    >🐺</PlayerTile>
+  )
+};
+
+export const Grid: React.FC = () => {
+  const grid = useSelector((state: RootState) => state.game.grid, eqGrid);
+  const wolves = useSelector((state: RootState) => state.game.world.wolves, eqWolves);
+  const hunters = useSelector((state: RootState) => state.game.world.hunters, eqHunters);
+
+  return (
+    <>
+      {wolves.map((wolf: Wolf) => <WolfTile
+        key={wolf.entity.id}
+        x={wolf.entity.position!!.x}
+        y={wolf.entity.position!!.y}
+      />)}
+      {hunters.map((hunter: Hunter) => <HunterTile
+        key={hunter.entity.id}
+        x={hunter.entity.position!!.x}
+        y={hunter.entity.position!!.y}
+        username={hunter.entity.user!!.name}
+      />)}
+      <table className="grid">
+        <tbody>
+        {
+          grid.map((row, rowIx) => <tr key={rowIx}>
+            {
+              row.map((cell, colIx) => <td key={colIx}>
+                {cell}
+              </td>)
+            }
+          </tr>)
+        }
+        </tbody>
+      </table>
+    </>
   );
 }
--- src/slices/game.slice.ts
+++ src/slices/game.slice.ts
@@ -1,18 +1,35 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {Namespacer} from "./Namespacer";
-import {Dictionary} from "../common/types";
+import _ from "lodash";
 
 export interface Position {
   y: number
   x: number
 }
 
+export interface User {
+  id: string
+  name: string
+}
+
+export interface Entity {
+  id: string
+  position?: Position
+  user?: User
+}
+
+export interface Player {
+  score: number
+}
+
 export interface Hunter {
-  pos: Position
+  player: Player
+  entity: Entity
 }
 
 export interface Wolf {
-  pos: Position
+  player: Player
+  entity: Entity
 }
 
 const namespace = 'game' as const
@@ -44,8 +61,8 @@ export type Grid
   = GridTile[][];
 
 export interface World {
-  wolves: Dictionary<Wolf>
-  hunters: Dictionary<Hunter>
+  wolves: Wolf[]
+  hunters: Hunter[]
 }
 
 export interface Room {
@@ -58,14 +75,64 @@ export interface User {
   name: string
 }
 
+export function eqEntity(e1: Entity, e2: Entity): boolean {
+  return e1.id === e2.id;
+}
+
+export function eqGrid(g1: Grid, g2: Grid): boolean {
+  return JSON.stringify(g1) === JSON.stringify(g2);
+}
+
+export function eqHunter(h1: Hunter, h2: Hunter): boolean {
+  return eqEntity(h1.entity, h2.entity);
+}
+
+export function eqWolf(w1: Wolf, w2: Wolf): boolean {
+  return eqEntity(w1.entity, w2.entity);
+}
+
+/**
+ * @param h1 sorted list of hunters
+ * @param h2 sorted list of hunters
+ */
+export function eqHunters(h1: Hunter[], h2: Hunter[]): boolean {
+  return h1.length === h2.length
+    && _.zip(h1, h2)
+      .reduce((acc: boolean, [h1, h2]: [Hunter|undefined, Hunter|undefined]): boolean =>
+      acc && eqHunter(h1!!, h2!!), true);
+}
+
+/**
+ * @param w1 sorted list of wolves
+ * @param w2 sorted list of wolves
+ */
+export function eqWolves(w1: Wolf[], w2: Wolf[]): boolean {
+  return w1.length === w2.length
+  && _.zip(w1, w2)
+      .reduce((acc: boolean, [w1, w2]: [Wolf|undefined, Wolf|undefined]): boolean =>
+      acc && eqWolf(w1!!, w2!!), true);
+}
+
+function compareHunter(h1: Hunter, h2: Hunter): number {
+  return compareEntity(h1.entity, h2.entity);
+}
+
+function compareWolf(w1: Wolf, w2: Wolf): number {
+  return compareEntity(w1.entity, w2.entity);
+}
+
+function compareEntity(e1: Entity, e2: Entity): number {
+  return e1.id.localeCompare(e2.id);
+}
+
 const slice = createSlice({
   name: namespace,
   initialState: {
     grid: [[]] as Grid,
     user: null as User|null,
     world: {
-      wolves: {},
-      hunters: {},
+      wolves: [],
+      hunters: [],
     } as World,
   },
   reducers: {},
@@ -85,7 +152,11 @@ const slice = createSlice({
     [actionIds.getWorldEnd]: (state, action: PayloadAction<GetWorldResponse>) => {
       return {
         ...state,
-        world: action.payload
+        world: {
+          ...state.world,
+          wolves: action.payload.wolves.sort(compareWolf),
+          hunters: action.payload.hunters.sort(compareHunter),
+        }
       };
     }
   }
